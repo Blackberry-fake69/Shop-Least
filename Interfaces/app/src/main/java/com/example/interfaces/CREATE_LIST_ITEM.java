78329a24357a47c5e20dcc3f2b1594c8c255095a
Blackberry <htztgg@gmail.com>
2023-09-13 15:36:54 -0400

    shop

---
+++ Interfaces/app/src/main/java/com/example/interfaces/CREATE_LIST_ITEM.java
@@ -0,0 +1,13 @@
+package com.example.interfaces;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CREATE_LIST_ITEM extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_list_item);
+    }
+}
